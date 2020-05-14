26238e718eb3fe13e9e5fd34fe7764858ffb0e59
Japneet Singh <singhjapneetssa@gmail.com>
2020-05-15 01:12:39 +0530

    Cards component works

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,109 +1,40 @@
 /* eslint-disable max-len,no-script-url,jsx-a11y/anchor-is-valid */
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
+import GridLayout from 'react-grid-layout';
+import jwala from './images/jwala.png';
 
+const layout = [
+    { i: 'a', x: 0, y: 0, w: 1, h: 2, static: true, isResizable: false },
+    { i: 'b', x: 1, y: 0, w: 1, h: 2, static: true, isResizable: false },
+];
 const HostelCard = () => (
-    <Card className="MuiPostCard--02">
-        <CardMedia
-            className="MuiCardMedia-root"
-            image="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/2000px-Git_icon.svg.png"
-        />
-        <CardContent className="MuiCardContent-root">
-            <Typography className="MuiTypography--date" variant="overline">
-                28 Mar 2019
-            </Typography>
-            <Typography
-                className="MuiTypography--heading"
-                variant="h6"
-                gutterBottom
+    <>
+        <div className="card-title">
+            <h1>Jwalamukhi Hostel</h1>
+        </div>
+        <div className="card-bg">
+        <h1 className="card-subtitle">Est. 1965</h1>
+            <GridLayout
+                className="layout"
+                layout={layout}
+                cols={2}
+                rowHeight={30}
+                width={200}
             >
-                What is Git ?
-            </Typography>
-            <Typography className="MuiTypography--subheading">
-                Git is a distributed version control system. Every dev has a
-                working copy of the code and...
-            </Typography>
-            <Button className="MuiButton--readMore">Read More</Button>
-        </CardContent>
-    </Card>
-);
+                <div key="a">
+                <img src={jwala} alt="Jwalamukhi Hostel" className="card-img" />
+                </div>
+                <div key="b">
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor {' '}
+            </p></div>
+            </GridLayout>
 
-HostelCard.getTheme = (muiBaseTheme) => ({
-    MuiCard: {
-        root: {
-            '&.MuiPostCard--02': {
-                borderRadius: muiBaseTheme.spacing.unit * 2, // 16px
-                transition: '0.3s',
-                boxShadow: '0px 14px 80px rgba(34, 35, 58, 0.2)',
-                width: '95%',
-                position: 'relative',
-                maxWidth: 800,
-                marginLeft: 'auto',
-                overflow: 'initial',
-                background: '#ffffff',
-                display: 'flex',
-                alignItems: 'center',
-                padding: `${muiBaseTheme.spacing.unit * 2}px 0`,
-                '&:hover': {
-                    transform: 'translateY(-3px)',
-                    boxShadow: '0 4px 20px 0 rgba(0,0,0,0.12)',
-                },
-                '& .MuiCardMedia-root': {
-                    flexShrink: 0,
-                    width: '40%',
-                    paddingTop: '48%',
-                    transform: 'translateX(-24%)',
-                    boxShadow: '4px 4px 20px 1px rgba(252, 56, 56, 0.2)',
-                    borderRadius: muiBaseTheme.spacing.unit * 2, // 16
-                    backgroundImage:
-                        'linear-gradient(147deg, #fe8a39 0%, #fd3838 74%)',
-                    backgroundColor: muiBaseTheme.palette.common.white,
-                    overflow: 'hidden',
-                    '&:after': {
-                        content: '" "',
-                        position: 'absolute',
-                        top: 0,
-                        left: 0,
-                        width: '100%',
-                        height: '100%',
-                        backgroundImage:
-                            'linear-gradient(147deg, #fe8a39 0%, #fd3838 74%)',
-                        borderRadius: muiBaseTheme.spacing.unit * 2, // 16
-                        opacity: 0.5,
-                    },
-                },
-                '& .MuiCardContent-root': {
-                    textAlign: 'left',
-                    paddingLeft: 0,
-                    padding: muiBaseTheme.spacing.unit * 2,
-                },
-                '& .MuiTypography--heading': {
-                    fontWeight: 'bold',
-                },
-                '& .MuiTypography--subheading': {
-                    marginBottom: muiBaseTheme.spacing.unit * 2,
-                },
-                '& .MuiButton--readMore': {
-                    backgroundImage:
-                        'linear-gradient(147deg, #fe8a39 0%, #fd3838 74%)',
-                    boxShadow: '0px 4px 32px rgba(252, 56, 56, 0.4)',
-                    borderRadius: 100,
-                    paddingLeft: 24,
-                    paddingRight: 24,
-                    color: '#ffffff',
-                },
-            },
-        },
-    },
-});
-HostelCard.metadata = {
-    name: 'Post Card II',
-    description: 'Personal Post',
-    credit: 'https://codemyui.com/gradient-card-ui-image-info-slider/',
-};
+            
+            
+        </div>
+    </>
+);
 
 export default HostelCard;
--- src/components/gridHostel.jsx
+++ src/components/gridHostel.jsx
@@ -1,48 +1,88 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import { Responsive, WidthProvider } from 'react-grid-layout';
+import HostelCard from './Card';
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 class MyFirstGrid extends React.Component {
     render() {
-        // layout is an array of objects, see the demo for more complete usage
-        const layout = [
-            { i: 'a', x: 0, y: 0, w: 3, h: 1 },
-            { i: 'b', x: 3, y: 0, w: 3, h: 1 },
-            { i: 'c', x: 6, y: 0, w: 3, h: 1 },
-            { i: 'd', x: 9, y: 0, w: 3, h: 1 },
-            { i: 'e', x: 0, y: 1, w: 3, h: 1 },
-            { i: 'f', x: 3, y: 1, w: 3, h: 1 },
-            { i: 'g', x: 6, y: 1, w: 3, h: 1 },
-            { i: 'h', x: 9, y: 1, w: 3, h: 1 },
-            { i: 'i', x: 9, y: 2, w: 3, h: 1 },
-            { i: 'j', x: 0, y: 2, w: 3, h: 1 },
-            { i: 'k', x: 3, y: 2, w: 3, h: 1 },
-            { i: 'l', x: 6, y: 2, w: 3, h: 1 },
-            { i: 'm', x: 0, y: 3, w: 3, h: 1 },
+        const layoutLg = [
+            {
+                i: 'a',
+                x: 0,
+                y: 0,
+                w: 3,
+                h: 2,
+                isResizable: false,
+                useCSSTransforms: true,
+            },
+            { i: 'b', x: 3, y: 0, w: 3, h: 2, isResizable: false },
+            { i: 'c', x: 6, y: 0, w: 3, h: 2, isResizable: false },
+            { i: 'd', x: 9, y: 0, w: 3, h: 2, isResizable: false },
+            { i: 'e', x: 0, y: 1, w: 3, h: 2, isResizable: false },
+            { i: 'f', x: 3, y: 1, w: 3, h: 2, isResizable: false },
+            { i: 'g', x: 6, y: 1, w: 3, h: 2, isResizable: false },
+            { i: 'h', x: 9, y: 1, w: 3, h: 2, isResizable: false },
+            { i: 'i', x: 9, y: 2, w: 3, h: 2, isResizable: false },
+            { i: 'j', x: 0, y: 2, w: 3, h: 2, isResizable: false },
+            { i: 'k', x: 3, y: 2, w: 3, h: 2, isResizable: false },
+            { i: 'l', x: 6, y: 2, w: 3, h: 2, isResizable: false },
+            { i: 'm', x: 0, y: 3, w: 3, h: 2, isResizable: false },
+            { i: 'n', x: 3, y: 3, w: 3, h: 2, isResizable: false },
+            { i: 'o', x: 6, y: 3, w: 3, h: 1, isResizable: false },
+            { i: 'p', x: 9, y: 4, w: 3, h: 1, isResizable: false },
+            { i: 'q', x: 0, y: 5, w: 3, h: 1, isResizable: false },
         ];
-        const layouts = { lg: layout };
+        const layouts = { lg: layoutLg };
         return (
             <ResponsiveGridLayout
                 className="layout"
                 layouts={layouts}
                 breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-                cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+                cols={{ lg: 12, md: 8, sm: 3, xs: 2, xxs: 1 }}
+                isResizable="true"
+                useCSSTransforms="true"
             >
-                <div key="a" className="hostelGrid" />
-                <div key="b" className="hostelGrid" />
-                <div key="c" className="hostelGrid" />
-                <div key="d" className="hostelGrid" />
-                <div key="e" className="hostelGrid" />
-                <div key="f" className="hostelGrid" />
-                <div key="g" className="hostelGrid" />
-                <div key="h" className="hostelGrid" />
-                <div key="i" className="hostelGrid" />
-                <div key="j" className="hostelGrid" />
-                <div key="k" className="hostelGrid" />
-                <div key="l" className="hostelGrid" />
-                <div key="m" className="hostelGrid" />
+                <div key="a" className="hostelGrid" isResizable="true">
+                    <HostelCard />
+                </div>
+                <div key="b" className="hostelGrid">
+                    <HostelCard />
+                </div>
+                <div key="c" className="hostelGrid">
+                    <HostelCard />
+                </div>
+                <div key="d" className="hostelGrid" isResizable="true">
+                    <HostelCard />
+                </div>
+                <div key="e" className="hostelGrid">
+                    <HostelCard />
+                </div>
+                <div key="f" className="hostelGrid">
+                    <HostelCard />
+                </div>
+                <div key="g" className="hostelGrid" isResizable="true">
+                    <HostelCard />
+                </div>
+                <div key="h" className="hostelGrid">
+                    <HostelCard />
+                </div>
+                <div key="i" className="hostelGrid">
+                    <HostelCard />
+                </div>
+                <div key="j" className="hostelGrid" isResizable="true">
+                    <HostelCard />
+                </div>
+                <div key="k" className="hostelGrid">
+                    <HostelCard />
+                </div>
+                <div key="l" className="hostelGrid">
+                    <HostelCard />
+                </div>
+                <div key="m" className="hostelGrid" isResizable="true">
+                    <HostelCard />
+                </div>
             </ResponsiveGridLayout>
         );
     }
